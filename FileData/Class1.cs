fecb181e9628e19c31f747c37d7c4e71f2b39b04
UmarSalem <umars62350@gmail.com>
2025-08-15 11:49:24 +0200

    Initial setup for the WebAPI

---
+++ FileData/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FileData
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Shared/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public User Owner { get; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Todo( User owner, string title) { Owner = owner; Title = title; }
+
+    }
+}
